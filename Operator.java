f588c3827cbcb7075ad7b28be6ed87f207afdc92
hongboo <791620392@qq.com>
2018-07-04 09:53:50 +0800

    运算

---
+++ Operator.java
@@ -0,0 +1,19 @@
+public class Operator {
+	
+	public static double addition (int a, int b) {
+		return a + b;
+	}
+
+	pubic static double subtraction(int a, int b) {
+		return a - b;
+	}  
+
+	public static double lication(int a, int b) {
+		return a * b;
+	}
+
+	public static double division(int a, int b) {
+		return a / b;
+	}
+
+}
